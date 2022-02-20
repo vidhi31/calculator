4523ebf3a40d34ffa9bf5d41f8361acf9592f0fa
vidhi31 <91669015+vidhi31@users.noreply.github.com>
2022-02-20 21:12:35 +0530

    js added

--- index.js
+++ index.js
@@ -23,13 +23,33 @@ buttons.forEach(function(btn){
         if (!btn.id.match('erase')) {
             // To display value on btn press
             ScreenValue.push(btn.value)
-            InputScreen.innerHTML = realTimeScreenValue.join('');
+            InputScreen.innerHTML = ScreenValue.join('');
 
             // To evaluate answer in real time
             if (btn.classList.contains('num_btn')) {
 
-                answerScreen.innerHTML = eval(ScreenValue.join(''));
-
+                AnswerScreen.innerHTML = eval(ScreenValue.join(''));
             }
-
         }
+
+ // When erase button is clicked
+ if (btn.id.match('erase')) {
+    ScreenValue.pop();
+    InputScreen.innerHTML = ScreenValue.join('');
+    AnswerScreen.innerHTML = eval(ScreenValue.join(''));
+}
+
+// When clicked button is evaluate button
+if (btn.id.match('equalTo')) {
+    InputScreen.className = 'AnswerScreen';
+    AnswerScreen.className = 'InputScreen';
+    AnswerScreen.style.color = "white";
+}
+
+// To prevent undefined error in screen
+if (typeof eval(ScreenValue.join('')) == 'undefined') {
+    AnswerScreen.innerHTML = 0
+}
+
+})
+})
