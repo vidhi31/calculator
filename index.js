8aeea49077355ac47b2c2e9ffc58cfe0c3322bea
vidhi31 <91669015+vidhi31@users.noreply.github.com>
2022-02-20 18:37:41 +0530

    css added to display

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-document.addEventListener("DOMContentLoaded",function onreadyhandler(){
-    var keyboardElement= document.getElementsByClassName("keyboard");
-    for (var i = 0 ; i < keyboardElement.length; i++) {
-        keyboardElement[i].addEventListener("click",function handler(event){
-            var clickedevent=event.target.innerText;
-            document.getElementById("screen").value = clickedevent;
-            console.log(clickedevent);
-            console.log("working");});
-        }
-});      
+    
